b9d611fe1f6790a6d88139c0d4f1d3032ed62165
Nathan Hering <nhering@hotmail.com>
2021-10-02 19:00:36 -0400

    211002

--- posts/2021/211002.js
+++ posts/2021/211002.js
@@ -2,11 +2,28 @@ content.innerHTML = `\
     <h1>Pellet Stove & Lawnmower</h1>\
     <h5>Saturday October 2 2021</h5>\
     <br>\
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lorem mauris.</p>\
+    <p>A litteral house warming gift and rust proofing the mower deck.</p>\
+    <br>\
+    <p>My brother came by to deliver a very nice house warming gift that is very litteraly meant \
+    to warm the house. He got me a ton of wood pellets. This was such a nice gesture and it helped \
+    me get a sense of how long it would take to unload a pallet of them. I just ordered four tons \
+    of the same brand and wasn't sure how much work it would be.</p>\
+    <br>\
+    <p>When they deliver the pellets they may be able to set them in the garage but I don't know \
+    what kind of equipment they have for unloading them off of the truck. So they may end up leaving \
+    them in the driveway for me to manually move them into the garage.</p>\
+    <br>\
+    <p>While he was here, he helped me remove the mower deck from my new lawn tractor and coat all the \
+    rust prone areas with fluid film. Once we finished with that, we connected the battery, added oil, 
+    and fueled it up. Then we fired it up for a test run. I love it! The hydrostatic transmission is \
+    so nice. And it can really move. I'm going to enjoy mowing the lawn instead of looking at is as a \
+    chore.</p>\
     <br>\
     <div class='img-caption'>\
         <img src='./images/2021/10/20211002_132500.jpg'/ >\
         <div class='caption'>The mower deck being treated and the ton of pellets my brother got \
         for me as a house warming gift.</div>\
     </div>\
+    <p></p>\
+    <br>\
 `
