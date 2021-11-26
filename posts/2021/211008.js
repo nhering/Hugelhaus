caacabeb40bc18177fcb93b81e54a728fc2e84a6
Nathan Hering <nathanh@gointerject.com>
2021-11-26 09:55:56 -0500

    211008

---
+++ posts/2021/211008.js
@@ -0,0 +1,22 @@
+content.innerHTML = `\
+    <h1>Pellet Deleviery</h1>\
+    <h5>Monday October 08 2021</h5>\
+    <br>\
+    <p>I ordered five more tons of pellets.</p>\
+    <br>/\
+    <p>It took about five weeks from the time I placed the order before they \
+    were able to deliver them. I spoke with the owner at JMJ who informed me that \
+    they prioritize furnace installs over pellet orders. That makes sense, and \
+    I wasn't in a big hurry anyway since I already had one ton.</p> \
+    <br>/\
+    <p>When they delivered, it was the owner and an employee. The flat bed truck \
+    had capacity for six tons so this was almost a full load. They had a pallet jack \
+    and a portable fork-lift. They a pallet off the truck and were able to just barely \
+    set it inside the garage. Then they used the jack to position it.
+    <br>/\
+    <div class='img-caption'>\
+        <img src='./images/2021/11/20211126_093851.jpg'/ >\
+        <div class='caption'>This takes up nearly half of the garage. \
+        Hopefully that will be more than enough to get me through the winter.</div>\
+    </div>\
+`
--- scripts/menu.js
+++ scripts/menu.js
@@ -391,7 +391,7 @@ const posts = [
         src: './posts/2021/210912.js',
     },
     {   id: 210925, year: 2021, month: 9, day: 25,
-        menuText: "Test drive",
+        menuText: "Test Drive",
         tags: [
             tags.MOVE_IN
         ],
@@ -400,7 +400,7 @@ const posts = [
     {   id: 211002, year: 2021, month: 10, day: 2,
         menuText: "Pellet Stove & Lawnmower",
         tags: [
-            tags.MOVE_IN
+            tags.MOVE_IN, tags.UTILITIES
         ],
         src: './posts/2021/211002.js',
     },
@@ -411,11 +411,75 @@ const posts = [
         ],
         src: './posts/2021/211003.js',
     },
+    {
+        id: 211008, year: 2021, month: 10, day: 8,
+        menuText: "Pellet Deleviery",
+        tags: [
+            tags.UTILITIES, tags.GARAGE
+        ],
+        src: './posts/2021/211008.js',
+    },
     {   id: 211015, year: 2021, month: 10, day: 15,
-        menuText: "First week",
+        menuText: "First Week",
         tags: [
             tags.CLEAN_UP, tags.YARD, tags.IMPROVEMENTS, tags.MOVE_IN
         ],
         src: './posts/2021/211015.js',
-    }
+    },
+    // {
+    //     id: 211110, year: 2021, month: 11, day: 10,
+    //     menuText: "Ambience Outside And In",
+    //     tags: [
+    //         tags.LITTLE_MOMENTS
+    //     ],
+    //     src: './posts/2021/211110.js',
+    // },
+    // {
+    //     id: 211111, year: 2021, month: 11, day: 11,
+    //     menuText: "Water Treatment Install",
+    //     tags: [
+    //         tags.PLUMBING, tags.UTILITIES, tags.IMPROVEMENTS, tags.BASEMENT
+    //     ],
+    //     src: './posts/2021/211111.js',
+    // },
+    // {
+    //     id: 211114, year: 2021, month: 11, day: 14,
+    //     menuText: "First Snow",
+    //     tags: [
+    //         tags.LITTLE_MOMENTS, tags.YARD, tags.LIVINGROOM
+    //     ],
+    //     src: './posts/2021/211114.js',
+    // },
+    // {
+    //     id: 211116, year: 2021, month: 11, day: 16,
+    //     menuText: "Winterizing",
+    //     tags: [
+    //         tags.BATHROOM, tags.BEDROOM, tags.LIVINGROOM, tags.UTILITIES
+    //     ],
+    //     src: './posts/2021/211116.js',
+    // },
+    // {
+    //     id: 211117, year: 2021, month: 11, day: 17,
+    //     menuText: "Another Burn",
+    //     tags: [
+    //         tags.CLEAN_UP, tags.IMPROVEMENTS, tags.MOVE_IN
+    //     ],
+    //     src: './posts/2021/211117.js',
+    // },
+    // {
+    //     id: 211122, year: 2021, month: 11, day: 22,
+    //     menuText: "",
+    //     tags: [
+    //         tags.CLEAN_UP, tags.IMPROVEMENTS, tags.MOVE_IN
+    //     ],
+    //     src: './posts/2021/211122.js',
+    // },
+    // {
+    //     id: 211123, year: 2021, month: 11, day: 23,
+    //     menuText: "Generator",
+    //     tags: [
+    //         tags.UTILITIES
+    //     ],
+    //     src: './posts/2021/211123.js',
+    // }
 ]
