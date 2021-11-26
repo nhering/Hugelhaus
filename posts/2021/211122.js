268f505322ee51056dd25edb3784c1be371689ff
Nathan Hering <nathanh@gointerject.com>
2021-11-26 18:19:06 -0500

    211122

---
+++ posts/2021/211122.js
@@ -0,0 +1,19 @@
+content.innerHTML = `\
+    <h1>Recycle Metal Items Left Behind</h1>\
+    <h5>Monday November 22 2021</h5>\
+    <br>\
+    <p>At least 700lbs of metal was left behind.</p>\
+    <br>\
+    <p>Between some items that were left in the house and things I dragged out of the \
+    weeds at the edges of the lawn, there was a lot of recyclable metal. Since I didn't \
+    have any use for any of these things, I asked my brother to help me take them to a \
+    scrap yard where they would give me some money to recycle the metal.</p>\
+    <br>\
+    <p>We loaded it up and it barely fit in his truck. The dry cleaner garment convery \
+    components probably weighed the most out of all of it. It ended up weighing roughly \
+    700 pounds and they gave my about $68 for it.</p>\
+    <br>\
+    <p>There is still some stuff burried in the weeds that I can't get to until I start \
+    cutting back the overgrowth. I'm not exactly sure it'll be enough to merit another \
+    run to the recycle place. We'll see.</p>\
+`
--- scripts/menu.js
+++ scripts/menu.js
@@ -437,42 +437,42 @@ const posts = [
         ],
         src: './posts/2021/211015.js',
     },
-    {    id: 211110, year: 2021, month: 11, day: 10,
+    {   id: 211110, year: 2021, month: 11, day: 10,
         menuText: "Scenic Moments",
         tags: [
             tags.LITTLE_MOMENTS
         ],
         src: './posts/2021/211110.js',
     },
-    {    id: 211111, year: 2021, month: 11, day: 11,
+    {   id: 211111, year: 2021, month: 11, day: 11,
         menuText: "Water Treatment Install",
         tags: [
             tags.PLUMBING, tags.UTILITIES, tags.IMPROVEMENTS, tags.BASEMENT
         ],
         src: './posts/2021/211111.js',
     },
-    {    id: 211116, year: 2021, month: 11, day: 16,
+    {   id: 211116, year: 2021, month: 11, day: 16,
         menuText: "Winterizing",
         tags: [
             tags.BATHROOM, tags.BEDROOM, tags.LIVINGROOM, tags.UTILITIES
         ],
         src: './posts/2021/211116.js',
     },
-    {    id: 211117, year: 2021, month: 11, day: 17,
+    {   id: 211117, year: 2021, month: 11, day: 17,
         menuText: "Another Burn",
         tags: [
             tags.CLEAN_UP, tags.MOVE_IN
         ],
         src: './posts/2021/211117.js',
     },
-    // {    id: 211122, year: 2021, month: 11, day: 22,
-    //     menuText: "",
-    //     tags: [
-    //         tags.CLEAN_UP, tags.IMPROVEMENTS, tags.MOVE_IN
-    //     ],
-    //     src: './posts/2021/211122.js',
-    // },
-    // {    id: 211123, year: 2021, month: 11, day: 23,
+    {   id: 211122, year: 2021, month: 11, day: 22,
+        menuText: "Recycle Metal Items Left Behind",
+        tags: [
+            tags.CLEAN_UP, tags.MOVE_IN
+        ],
+        src: './posts/2021/211122.js',
+    },
+    // {   id: 211123, year: 2021, month: 11, day: 23,
     //     menuText: "Generator",
     //     tags: [
     //         tags.UTILITIES
