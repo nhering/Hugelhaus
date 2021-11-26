3b9596073ba1e7f3044e130323232a6601fc9cf1
Nathan Hering <nathanh@gointerject.com>
2021-11-26 15:13:50 -0500

    211008

--- posts/2021/211008.js
+++ posts/2021/211008.js
@@ -3,17 +3,17 @@ content.innerHTML = `\
     <h5>Monday October 08 2021</h5>\
     <br>\
     <p>I ordered five more tons of pellets.</p>\
-    <br>/\
+    <br>\
     <p>It took about five weeks from the time I placed the order before they \
     were able to deliver them. I spoke with the owner at JMJ who informed me that \
     they prioritize furnace installs over pellet orders. That makes sense, and \
     I wasn't in a big hurry anyway since I already had one ton.</p> \
-    <br>/\
+    <br>\
     <p>When they delivered, it was the owner and an employee. The flat bed truck \
     had capacity for six tons so this was almost a full load. They had a pallet jack \
     and a portable fork-lift. They a pallet off the truck and were able to just barely \
-    set it inside the garage. Then they used the jack to position it.
-    <br>/\
+    set it inside the garage. Then they used the jack to position it.</p> \
+    <br>\
     <div class='img-caption'>\
         <img src='./images/2021/11/20211126_093851.jpg'/ >\
         <div class='caption'>This takes up nearly half of the garage. \
