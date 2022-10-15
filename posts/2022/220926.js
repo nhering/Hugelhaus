52385b25a20ffec61d904eb29c54d8f167d8094a
Nathan Hering <nathanh@gointerject.com>
2022-10-15 14:41:50 -0400

    post 220926

---
+++ posts/2022/220926.js
@@ -0,0 +1,91 @@
+let slides_220926a = JSON.stringify([
+    ['/images/2021/08/20210821_114014.jpg','Facing Southwest: The brush along the fence was thickest about half way down the hill.'],
+    ['/images/2021/08/20210821_114016.jpg','Facing West: There was a fallen oak tree which had years of growth around it.'],
+    ['/images/2021/08/20210821_114017.jpg','Facing Northwest: There is a cage completely covered in weeds here and behind it a fallen oak tree \
+    with a whole lot of growth around it.'],
+    ['/images/2021/08/20210821_114019.jpg','Facing Northwest: There is a huge amount of weeds all around a few trees at the back of the yard.'],
+    ['/images/2021/08/20210821_114021.jpg','Facing North: The fence is completely hidden. There is a gate here and a large wooden crate-like object in front of it that is completely covered in years of growth.'],
+    ['/images/2021/08/20210821_114022.jpg','Facing North: This fire pit was so overgrown that it was impossible to see just how much trash was burried in it.'],
+    ['/images/2021/08/20210821_114024.jpg','Facing Northeast'],
+    ['/images/2021/08/20210821_114025.jpg','Facing Northeast'],
+    ['/images/2021/08/20210821_114027.jpg','Facing East'],
+    ['/images/2021/08/20210821_114029.jpg','Facing Southeast'],
+    ['/images/2021/08/20210821_114030.jpg','Facing Southeast'],
+    ['/images/2021/08/20210821_114032.jpg','Facing South: You can barely see it, but in the shade on the hill are the lids to the septic tanks. These are difficult to mow around. It looks like the previous owner hit one of the lids at some point.'],
+    ['/images/2021/08/20210821_114034.jpg','Facing South']
+])
+
+let slides_220926b = JSON.stringify([
+    ['/images/2022/09/20220927_093805.jpg','From the NE corner facing south.'],
+    ['/images/2022/09/20220927_093808.jpg','From the NE corner facing southeast.'],
+    ['/images/2022/09/20220927_093811.jpg','From the NE corner facing west.'],
+
+    ['/images/2022/09/20220927_093942.jpg','From the NW corner facing east.'],
+    ['/images/2022/09/20220927_093945.jpg','From the NW corner facing southeast.'],
+    ['/images/2022/09/20220927_093955.jpg','From the NW corner facing west.'],
+
+    ['/images/2022/09/20220927_094108.jpg','From the SW corner facing north.'],
+    ['/images/2022/09/20220927_094113.jpg','From the SW corner facing northeast.'],
+    ['/images/2022/09/20220927_094118.jpg','From the SW corner facing west.'],
+
+    ['/images/2022/09/20220927_094221.jpg','From the SE corner facing east.'],
+    ['/images/2022/09/20220927_094224.jpg','From the SE corner facing northeast.'],
+    ['/images/2022/09/20220927_094226.jpg','From the SE corner facing north.'],
+
+    ['/images/2022/09/20220926_150324.jpg','From the livingroom, I caught a picture of a rainbow.']
+])
+
+content.innerHTML = `\
+    <h1>Yard Update</h1>\
+    <h5>Monday September 26 2022</h5>\
+    <br>\
+    <p>A little work at a time made a big difference.</p>\
+    <br>\
+    <p>I set out a few goals as far as yard work was concerned. I'll list \
+    them below these 'before' pictures. These are all taken from \
+    one position roughly in the middle of the yard. </p>\
+    <br>\
+    <h3>August 2021</h3>\
+    <br>\
+    <div class='slides' data-sources='${slides_220926a}'></div>\
+    <br>\
+    <br>\
+    <p>Here are the tasks I had on my list for the first year.</p><br>\
+    <ul>\
+    <li>Clear out some of the overgrowth by the fence to the west.</li><br>\
+    <li>Remove the rusty cage and all the weeds etc around it.</li><br>\
+    <li>Remove the fallen oak tree.</li><br>\
+    <li>Clear out the weeds from around the trees at the north end of the yard.</li><br>\
+    <li>Clear out the brush and crate by the gate.</li><br>\
+    <li>Clean the burn pit and maybe build a landscaping stone wall around it.</li><br>\
+    <li>Place mulch around the septic tank lids so I can start growing ground \
+    cover in spring of 2023.</li><br>\
+    <li>Remove the white fence by the garage.</li><br>\
+    <li>Take down the satelite dishes.</li>\
+    </ul>\
+    <br>\
+    <p>I may not have accomplished all the items on the list, but I did get a lot \
+    done and the yard is definately starting to look a lot more like I envision for \
+    the future. I also did a couple things with the front yard but I didn't document \
+    them as well.</p>\
+    <br><br>\
+    <h3>September 2022</h3>\
+    <br>\
+    <div class='slides' data-sources='${slides_220926b}'></div>\
+    <br>\
+    <p>Here's what I have in mind for next year.</p>\
+    <br>\
+    <ul>\
+    <li>Remove the white fence by the garage. (from last year's list)</li><br>\
+    <li>Take down the satelite dishes. (from last year's list)</li><br>\
+    <li>Continue clearing out the weeds from around the trees at the north end of the \
+    yard. I may leave only the trees I intend to harvest cuttings from.</li><br>\
+    <li>Clear out the brush and weeds along the north fence and possibly the east and \
+    west too.</li><br>\
+    <li>I'm considering mulching along the north fence like I did with the septic tank lids.</li><br>\
+    <li>Mulch around the leach field lids in the front yard.</li><br>\
+    <li>Possibly build a decorative wooden fence by the leach field lids.</li><br>\
+    <li>Start growing ground cover around the septic tank lids.</li><br>\
+    <li>Clear out the growth around the fence at the south of the yard and mulch.</li><br>\
+    </ul>\
+`
--- scripts/posts.js
+++ scripts/posts.js
@@ -4,223 +4,230 @@ const posts = [
         tags: [
             tags.MOVE_IN
         ],
-        src: './posts/2021/210605.js',
+        src: './posts/2021/210605.js'
     },
     {   id: 210619, year: 2021, month: 6, day: 19,
         menuText: "Home Inspection",
         tags: [
             tags.MOVE_IN
         ],
-        src: './posts/2021/210619.js',
+        src: './posts/2021/210619.js'
     },
     {   id: 210720, year: 2021, month: 7, day: 20,
         menuText: "Title Signing",
         tags: [
             tags.MOVE_IN
         ],
-        src: './posts/2021/210720.js',
+        src: './posts/2021/210720.js'
     },
     {   id: 210809, year: 2021, month: 8, day: 9,
         menuText: "The Keys",
         tags: [
             tags.MOVE_IN
         ],
-        src: './posts/2021/210809.js',
+        src: './posts/2021/210809.js'
     },
     {   id: 210813, year: 2021, month: 8, day: 13,
         menuText: "Left Behind",
         tags: [
             tags.CLEAN_UP, tags.MOVE_IN
         ],
-        src: './posts/2021/210813.js',
+        src: './posts/2021/210813.js'
     },
     {   id: 210814, year: 2021, month: 8, day: 14,
         menuText: "Utilities",
         tags: [
             tags.UTILITIES, tags.MOVE_IN
         ],
-        src: './posts/2021/210814.js',
+        src: './posts/2021/210814.js'
     },
     {   id: 210816, year: 2021, month: 8, day: 16,
         menuText: "Water Pressure",
         tags: [
             tags.BASEMENT, tags.PLUMBING, tags.REPAIRS
         ],
-        src: './posts/2021/210816.js',
+        src: './posts/2021/210816.js'
     },
     {   id: 210818, year: 2021, month: 8, day: 18,
         menuText: "New Lawnmower",
         tags: [
             tags.MOVE_IN, tags.YARD
         ],
-        src: './posts/2021/210818.js',
+        src: './posts/2021/210818.js'
     },
     {   id: 210906, year: 2021, month: 9, day: 6,
         menuText: "House Warming",
         tags: [
             tags.FRIENDS_FAMILY, tags.MOVE_IN
         ],
-        src: './posts/2021/210906.js',
+        src: './posts/2021/210906.js'
     },
     {   id: 210912, year: 2021, month: 9, day: 12,
         menuText: "What Feels Like Home?",
         tags: [
             tags.LITTLE_MOMENTS ,tags.MOVE_IN
         ],
-        src: './posts/2021/210912.js',
+        src: './posts/2021/210912.js'
     },
     {   id: 210925, year: 2021, month: 9, day: 25,
         menuText: "Test Drive",
         tags: [
             tags.MOVE_IN
         ],
-        src: './posts/2021/210925.js',
+        src: './posts/2021/210925.js'
     },
     {   id: 211002, year: 2021, month: 10, day: 2,
         menuText: "Pellet Stove & Lawnmower",
         tags: [
             tags.MOVE_IN, tags.UTILITIES
         ],
-        src: './posts/2021/211002.js',
+        src: './posts/2021/211002.js'
     },
     {   id: 211003, year: 2021, month: 10, day: 3,
         menuText: "Smoke 'em Out",
         tags: [
             tags.CLEAN_UP
         ],
-        src: './posts/2021/211003.js',
+        src: './posts/2021/211003.js'
     },
     {   id: 211008, year: 2021, month: 10, day: 8,
         menuText: "Pellet Deleviery",
         tags: [
             tags.UTILITIES, tags.GARAGE
         ],
-        src: './posts/2021/211008.js',
+        src: './posts/2021/211008.js'
     },
     {   id: 211015, year: 2021, month: 10, day: 15,
         menuText: "First Week",
         tags: [
             tags.CLEAN_UP, tags.YARD, tags.IMPROVEMENTS, tags.MOVE_IN
         ],
-        src: './posts/2021/211015.js',
+        src: './posts/2021/211015.js'
     },
     {   id: 211110, year: 2021, month: 11, day: 10,
         menuText: "Scenic Moments",
         tags: [
             tags.LITTLE_MOMENTS
         ],
-        src: './posts/2021/211110.js',
+        src: './posts/2021/211110.js'
     },
     {   id: 211111, year: 2021, month: 11, day: 11,
         menuText: "Water Treatment Install",
         tags: [
             tags.PLUMBING, tags.UTILITIES, tags.IMPROVEMENTS, tags.BASEMENT
         ],
-        src: './posts/2021/211111.js',
+        src: './posts/2021/211111.js'
     },
     {   id: 211116, year: 2021, month: 11, day: 16,
         menuText: "Winterizing",
         tags: [
             tags.BATHROOM, tags.BEDROOM, tags.LIVINGROOM, tags.UTILITIES
         ],
-        src: './posts/2021/211116.js',
+        src: './posts/2021/211116.js'
     },
     {   id: 211117, year: 2021, month: 11, day: 17,
         menuText: "Another Burn",
         tags: [
             tags.CLEAN_UP, tags.MOVE_IN
         ],
-        src: './posts/2021/211117.js',
+        src: './posts/2021/211117.js'
     },
     {   id: 211122, year: 2021, month: 11, day: 22,
         menuText: "Recycle Metal Items Left Behind",
         tags: [
             tags.CLEAN_UP, tags.MOVE_IN
         ],
-        src: './posts/2021/211122.js',
+        src: './posts/2021/211122.js'
     },
     {   id: 211123, year: 2021, month: 11, day: 23,
         menuText: "Generator",
         tags: [
             tags.ELECTRIC ,tags.UTILITIES
         ],
-        src: './posts/2021/211123.js',
+        src: './posts/2021/211123.js'
     },
     {   id: 211205, year: 2021, month: 12, day: 05,
         menuText: "Scratching Post",
         tags: [
             tags.ANIMALS, tags.LIVINGROOM
         ],
-        src: './posts/2021/211205.js',
+        src: './posts/2021/211205.js'
     },
     {   id: 211212, year: 2021, month: 12, day: 12,
         menuText: "Stray Cat",
         tags: [
             tags.ANIMALS, tags.PROJECTS
         ],
-        src: './posts/2021/211212.js',
+        src: './posts/2021/211212.js'
     },
     {   id: 220206, year: 2022, month: 02, day: 06,
         menuText: "Recreation Fail",
         tags: [
             tags.LITTLE_MOMENTS, tags.LIVINGROOM
         ],
-        src: './posts/2022/220206.js',
+        src: './posts/2022/220206.js'
     },
     {   id: 220401, year: 2022, month: 04, day: 01,
         menuText: "Burn Pile Upgrade",
         tags: [
             tags.CLEAN_UP, tags.IMPROVEMENTS, tags.YARD
         ],
-        src: './posts/2022/220401.js',
+        src: './posts/2022/220401.js'
     },
     {   id: 220417, year: 2022, month: 04, day: 17,
         menuText: "Cat Barrier",
         tags: [
             tags.IMPROVEMENTS
         ],
-        src: './posts/2022/220417.js',
+        src: './posts/2022/220417.js'
     },
     {   id: 220430, year: 2022, month: 04, day: 30,
         menuText: "Clearing The Gate",
         tags: [
             tags.CLEAN_UP, tags.IMPROVEMENTS, tags.YARD
         ],
-        src: './posts/2022/220430.js',
+        src: './posts/2022/220430.js'
     },
     {   id: 220503, year: 2022, month: 05, day: 03,
         menuText: "Starlink",
         tags: [
             tags.UTILITIES
         ],
-        src: './posts/2022/220503.js',
+        src: './posts/2022/220503.js'
     },
     {   id: 220505, year: 2022, month: 05, day: 05,
         menuText: "Fill Dirt",
         tags: [
             tags.CLEAN_UP, tags.IMPROVEMENTS, tags.YARD
         ],
-        src: './posts/2022/220505.js',
+        src: './posts/2022/220505.js'
     },
     {   id: 220521, year: 2022, month: 05, day: 21,
         menuText: "Fallen Tree",
         tags: [
             tags.LITTLE_MOMENTS, tags.YARD
         ],
-        src: './posts/2022/220521.js',
+        src: './posts/2022/220521.js'
     },
     {   id: 220522, year: 2022, month: 05, day: 22,
         menuText: "A Mouse",
         tags: [
             tags.LITTLE_MOMENTS, tags.ANIMALS
         ],
-        src: './posts/2022/220522.js',
+        src: './posts/2022/220522.js'
     },
     {   id: 220605, year: 2022, month: 06, day: 05,
         menuText: "Germinating Mint",
         tags: [
             tags.PROJECTS, tags.YARD
         ],
-        src: './posts/2022/220605.js',
+        src: './posts/2022/220605.js'
+    },
+    {   id: 220926, year: 2022, month: 09, day: 26,
+        menuText: "Yard Update",
+        tags: [
+            tags.PROJECTS, tags.YARD
+        ],
+        src: './posts/2022/220926.js'
     }
 ]
