d2be6a8b73f5d01c2041c1f2634c25d9df4cdb7b
Nathan Hering <nathanh@gointerject.com>
2023-12-28 08:43:57 -0500

    add post que

---
+++ post_queue/snowdrop-flower/230401.js
@@ -0,0 +1,12 @@
+let slides_230303 = JSON.stringify([
+    ['/images/2023/03/.jpg','']
+])
+
+content.innerHTML = `\
+    <h1></h1>\
+    <h5>dddd mmmm dd yyyy</h5>\
+    <br>\
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lorem mauris.</p>\
+    <br>\
+    <div class='slides' data-sources='${slides_230303}'></div>
+`
---
+++ post_queue/yume/230607.js
@@ -0,0 +1,14 @@
+let slides_230607 = JSON.stringify([
+    ['/images/2023/06/.jpg','caption']
+])
+
+content.innerHTML = `\
+    <h1>Yume!</h1>\
+    <h5>Tuesday June 06 2023</h5>\
+    <br>\
+    <p>We brought home a new friend from Rescue Village</p>\
+    <br>\
+    <p>It's been a few months since </p>\
+    <br>\
+    <div class='slides' data-sources='${slides_000000}'></div>
+`
